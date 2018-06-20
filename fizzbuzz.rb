54e4f10e678a4efb76470aff2c76087a6d8143a5
Grace Govan <ggovan11@outlook.com>
2018-06-20 15:18:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-if(int%3==0)
-  puts "Fizz"
+ifint%3==0 && x%5==0)
+  puts "FizzBuzz"
 elsif(int%5==0)
   puts "Buzz"
-elsif(int%3==0 && x%5==0)
-  puts "FizzBuzz"
+elsif(int%3==0)
+  puts "Fizz"
   end
 end
 
-fizzbuzz(54)
+fizzbuzz(5)
