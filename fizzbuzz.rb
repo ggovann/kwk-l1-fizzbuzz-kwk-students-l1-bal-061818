d6037816f9a3b4ee1367e4b44352c53cbf76870d
Grace Govan <ggovan11@outlook.com>
2018-06-20 15:20:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-if(int%3==0)
+if (int%3==0 && x%5==0)
+  puts "FizzBuzz"
+elsif(int%3==0)
   puts "Fizz"
 elsif(int%5==0)
   puts "Buzz"
-elsif(int%3==0 && x%5==0)
-  puts "FizzBuzz"
   end
 end
 
-fizzbuzz(54)
+fizzbuzz(5)
